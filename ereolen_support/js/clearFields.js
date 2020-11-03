aa1d98a547e608dd4e5f4a946e5887cc251a839d
Martin Granath <martingranath@mac.local>
2020-11-03 13:36:59 +0100

    ER-1014: Changed stuff after code review

--- ereolen_support/js/clearFields.js
+++ ereolen_support/js/clearFields.js
@@ -7,9 +7,9 @@
     attach: function (context, settings) {
       'use strict';
 
-      let problem = document.getElementById("edit-ereolen-support-problem");
       clearFields();
 
+      let problem = document.getElementById("edit-ereolen-support-problem");
       problem.addEventListener('change', function(){
         clearFields();
       });
@@ -20,24 +20,28 @@
    * Clear fields.
    */
   function clearFields() {
-    let textFields = document.querySelectorAll('.form-type-textfield input');
-    let selectFields = document.querySelectorAll('.form-type-select select');
-    let textAreas = document.querySelectorAll('.form-type-textarea textarea');
     // Clear most textfields.
+    let textFields = document.querySelectorAll('.form-type-textfield input');
     for (i = 0; i < textFields.length; ++i) {
-      if(textFields[i].id !== 'edit-ereolen-support-name'
-        && textFields[i].id !== 'edit-ereolen-support-date-datepicker-popup-0') {
+      if(textFields[i].id !== 'edit-ereolen-support-name' &&
+        textFields[i].id !== 'edit-ereolen-support-date-datepicker-popup-0')
+      {
         textFields[i].value = "";
       }
     }
+
     // Clear most select fields.
+    let selectFields = document.querySelectorAll('.form-type-select select');
     for (i = 0; i < selectFields.length; ++i) {
-      if(selectFields[i].id !== 'edit-ereolen-support-problem'
-        && selectFields[i].id !== 'edit-ereolen-support-library') {
+      if(selectFields[i].id !== 'edit-ereolen-support-problem' &&
+        selectFields[i].id !== 'edit-ereolen-support-library')
+      {
         selectFields[i].value = "_none";
       }
     }
+
     // Clear all textareas.
+    let textAreas = document.querySelectorAll('.form-type-textarea textarea');
     for (i = 0; i < textAreas.length; ++i) {
       textAreas[i].value = "";
     }
