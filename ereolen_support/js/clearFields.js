6ee7090449c0d155f21d290e6617b8dde6f6f9c1
Mikkel Ricky <rimi@aarhus.dk>
2024-04-29 10:49:41 +0200

    1212: Removed clearing of support id

--- ereolen_support/js/clearFields.js
+++ ereolen_support/js/clearFields.js
@@ -21,7 +21,8 @@
     // Clear most textfields.
     let textFields = document.querySelectorAll('.form-type-textfield input');
     for (i = 0; i < textFields.length; ++i) {
-      if(textFields[i].id !== 'edit-ereolen-support-name' &&
+      if(textFields[i].id !== 'edit-ereolen-support-id' &&
+         textFields[i].id !== 'edit-ereolen-support-name' &&
         textFields[i].id !== 'edit-ereolen-support-date-datepicker-popup-0')
       {
         textFields[i].value = "";
